6001d507987e6a767fc3b8bab66d40a3371748a6
rhoshanaarif <125179706+rhoshanaarif@users.noreply.github.com>
2024-01-10 05:38:22 +0530

    made small change

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Rhoshan aarif';
+const name = 'Mr. Rhoshan aarif ';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
